ba3d91627c9ae5fb6b0d8ed4fbb67128680b4752
abdulbasit1345 <se22f-115@ssuet.edu.pk>
2024-11-07 20:03:02 +0500

    Add files via upload
    initial files

---
+++ SC-D_se314L/new_project2024/src/new_project2024/Main.java
@@ -0,0 +1,12 @@
+package new_project2024;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello this is Lab2 Introduction To Github.");
+		System.out.println("I will now upload this on github using eclipse IDE!!!");
+
+
+	}
+
+}
